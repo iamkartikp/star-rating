d1a44825890e649928566fa1ff841a4ebbb47115
Kartik <kartik.ipatil10@gmail.com>
2024-01-20 16:40:45 +0530

    Fix: TicTacToe

--- tictactoe/index.js
+++ tictactoe/index.js
@@ -1,12 +1,12 @@
 const squares = document.querySelectorAll(".game-square");
+const gameHeading = document.getElementById("game-heading");
 const restartBtn = document.getElementById("restart-button");
 
 const p1 = "X";
 const p2 = "O";
 
-let player = 1;
-
-let winner = null;
+let currentPlayer = p1;
+let isGameOver = false;
 
 const matrix = [
     [squares[0], squares[1], squares[2]],
@@ -15,48 +15,82 @@ const matrix = [
 ]
 
 squares.forEach((square) => {
-    square.addEventListener("click", makeMove);
+    square.addEventListener("click", handleClickBox)
 })
 
-function makeMove(e) {
-    if (player) {
-        e.currentTarget.innerText = p1;
-        e.currentTarget.disabled = true;
-        player = 0;
-    } else {
-        e.currentTarget.innerText = p2;
-        e.currentTarget.disabled = true;
-        player = 1;
-    }
+function handleClickBox() {
+    this.textContent = currentPlayer;
+    currentPlayer === p1 ? currentPlayer = p2 : currentPlayer = p1;
     checkWinner();
 }
 
 function checkWinner() {
-    for (let i = 0; i < 3; i++) {
-        if (matrix[i][0].innerText === matrix[i][1].innerText 
-            && matrix[i][1].innerText === matrix[i][2].innerText) {
-            winner = matrix[i][0];
+    for (let i = 0; i < matrix.length; i++) {
+        if (matrix[i][0].innerText !== "" && matrix[i][0].innerText === matrix[i][1].innerText && matrix[i][1].innerText === matrix[i][2].innerText) {
+            winner = matrix[i][0].innerText;
+            isGameOver = true;
             break;
         }
     }
 
-    for (let i = 0; i < 3; i++) {
-        if (matrix[0][i].innerText === matrix[1][i].innerText 
-            && matrix[1][i].innerText === matrix[2][i].innerText) {
-            winner = matrix[0][i];
+    for (let i = 0; i < matrix.length; i++) {
+        if (matrix[0][i].innerText !== "" && matrix[0][i].innerText === matrix[1][i].innerText && matrix[1][i].innerText === matrix[2][i].innerText) {
+            winner = matrix[0][i].innerText;
+            isGameOver = true;
             break;
         }
     }
 
-    if (matrix[0][0].innerText === matrix[1][1].innerText 
-        && matrix[1][1].innerText === matrix[2][2].innerText) {
-        winner = matrix[0][0];
-    }
-
-    if (matrix[0][2].innerText !== "" && matrix[0][2].innerText === matrix[1][1].innerText && matrix[1][1].innerText === matrix[2][0].innerText) {
-        winner = matrix[0][2];
+    if (matrix[0][0].innerText !== "" && matrix[0][0].innerText === matrix[1][1].innerText && matrix[1][1].innerText === matrix[2][2].innerText) {
+        winner = matrix[0][0].innerText;
         isGameOver = true;
     }
 
-    console.log(winner.innerText)
+    if (matrix[0][2].innerText !== "" && matrix[0][2].innerText === matrix[1][1].innerText && matrix[1][1].innerText === matrix[2][0].innerText) {
+        winner = matrix[0][2].innerText;
+        isGameOver = true;
+    }
+
+    if (isGameOver === false) {
+        let isTie = true;
+        for (let i = 0; i < matrix.length; i++) {
+            for (let j = 0; j < matrix.length; j++) {
+                if (matrix[i][j].innerText === "") {
+                    isTie = false;
+                    break;
+                }
+            }
+        }
+        if (isTie === true) {
+            gameHeading.innerText = "Tie Game!";
+            isGameOver = true;
+        }
+    }
+
+    if (isGameOver === true) {
+        if (winner === p1) {
+            gameHeading.innerText = "Player 1 Won!";
+        } else if (winner === p2) {
+            gameHeading.innerText = "Player 2 Won!";
+        }
+        for (let i = 0; i < matrix.length; i++) {
+            for (let j = 0; j < matrix.length; j++) {
+                if (matrix[i][j].innerText === "") {
+                    matrix[i][j].disabled = true;
+                }
+            }
+        }
+        restartBtn.style.display = "block";
+    }
+}
+
+restartBtn.addEventListener("click", handleRestartGame);
+
+function handleRestartGame() {
+    for (let r = 0; r < matrix.length; r++) {
+        for (let c = 0; c < matrix.length; c++) {
+            matrix[r][c].textContent = "";
+            matrix[r][c].disabled = false;
+        }
+    }
 }
